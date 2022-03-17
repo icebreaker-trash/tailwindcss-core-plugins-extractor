ca0b5cb3c054441f8411d8acdb8d6f8d39075ff2
SonOfMagic <1324318532@qq.com>
2022-03-18 00:38:12 +0800

    feat: commit codes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tailwindcss-core-plugins-extractor
+
+npm view tailwindcss version
+
+npm list --depth=0
+
+npm list -g --depth=0
--- scripts/check.js
+++ scripts/check.js
@@ -3,6 +3,32 @@ const packageJson = require('package-json')
 const semver = require('semver')
 const version = pkg.devDependencies.tailwindcss
 
+function getPackageVersion (args) {
+  const packageNames = typeof args === 'string'
+    ? args.split(' ')
+    : Array.isArray(args)
+      ? args
+      : false
+
+  if (!packageNames) {
+    throw new Error('No valid package names provided. Only string or array!')
+  }
+
+  const result = {}
+
+  packageNames.forEach(name => {
+    try {
+      result[name] = require(`${name}/package.json`).version
+    } catch (e) {
+      result[name] = null
+    }
+  })
+
+  return result
+}
+
+module.exports = getPackageVersion
+
 ;(async () => {
   const res = await packageJson('tailwindcss')
   const latestVersion = res.version
